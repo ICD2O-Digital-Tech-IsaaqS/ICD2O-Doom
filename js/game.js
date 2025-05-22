993a1eb526745155560a3b8356e950e05cb56e00
Isaaq Simon <IsaaqS>
2025-05-22 14:44:11 +0000

    RST

--- js/game.js
+++ js/game.js
@@ -1,15 +1,29 @@
+/* global Phaser */
+
 // Copyright (c) 2025 Isaaq Simon All rights reserved
 //
 // Created by: Isaaq Simon
 // Created on: May 2025
 // This file contains the JS functions for index.html
-/* global Phaser */
 
+  //* Game scene */
   const config = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
-    backgroundColor: 0x5f6e7a
+    physics: {
+      default: 'arcade',
+      arcade: {
+        debug: true
+      }
+    },
+    // set background color
+    backgroundColor: 0x5f6e7a,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      // We place it in the middle of the page.
+      autoCentre: Phaser.Scale.CENTRE_BOTH
+    }
   }
     
   const game = new Phaser.Game(config)
