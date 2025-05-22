22553303cabfce2f6652e73c5b9f310be01b1d29
Isaaq Simon <IsaaqS>
2025-05-22 14:14:43 +0000

    RST

---
+++ js/game.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 Isaaq Simon All rights reserved
+//
+// Created by: Isaaq Simon
+// Created on: May 2025
+// This file contains the JS functions for index.html
+/* global Phaser */
+
+  const config = {
+      type: Phaser.AUTO,
+      width: 1920,
+      height: 1080,
+      backgroundColor: add8e6
+  }
+    
+  const game = new Phaser.Game(config)
+  console.log(game)
