f6dec3b54b145a7c9b5fe7aced62f141759600d5
Isaaq Simon <IsaaqS>
2025-05-27 14:58:53 +0000

    Doom

--- js/splashScene.js
+++ js/splashScene.js
@@ -20,16 +20,6 @@ class SplashScene {
         this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
         this.splashSceneBackgroundImage.x = 1920 / 2
         this.splashSceneBackgroundImage.y = 1080 / 2
-    }
-  
-    update() {
-      const ctx = document.getElementById("gameCanvas").getContext("2d");
-      ctx.clearRect(0, 0, 800, 600);
-      ctx.fillStyle = "#000";
-      ctx.fillRect(0, 0, 800, 600);
-      ctx.fillStyle = "#fff";
-      ctx.font = "36px Arial";
-      ctx.fillText("Immaculata Presents...", 200, 300);
   
       // Transition to Title Scene after 3 seconds
       if (Date.now() - this.startTime > 3000) {
