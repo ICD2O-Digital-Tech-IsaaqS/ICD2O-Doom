16e847db329b905667da5d533e5844fa3b3b4e8e
Isaaq Simon <IsaaqS>
2025-06-03 14:56:05 +0000

    RST

--- js/gameScene.js
+++ js/gameScene.js
@@ -3,94 +3,99 @@
 // Created on: May 2025
 // This is the Game scene for the game
 
+
 /**
- * This class is the game scene for the game
+ * This class is the splash scene for the game
  */
 class GameScene extends Phaser.Scene {
-
-    // Create an alien with scaling health
+   
+    // create an alien
     createAlien() {
         const alienXLocation = Math.floor(Math.random() * 1920) + 1
         const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
-
         let alienXVelocity = Math.floor(Math.random() * 50) + 1
         alienXVelocity *= Math.round(Math.random()) ? 1 : -1
         anAlien.body.velocity.y = 200
         anAlien.body.velocity.x = alienXVelocity
-
-        anAlien.health = 1 + Math.floor(this.score / 25)
-
         this.alienGroup.add(anAlien)
     }
 
+
     constructor() {
         super({ key: 'gameScene' })
+
 
         this.ship = null
         this.fireMissile = false
         this.score = 0
         this.scoreText = null
-        this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center' }
+        this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', algin: 'centre' }
+
 
         this.gameOverText = null
-        this.gameOverTextStyle = { font: '65px Arial', fill: '#ff0000', align: 'center' }
+        this.gameOverTextStyle = { font: '65px Arial', fill: '#ff0000', algin: 'centre' }
+
 
         this.isGameOver = false
-        this.missileDamage = 1 // Each missile deals 1 damage
     }
-
+ 
     init(data) {
-        this.cameras.main.setBackgroundColor("0x5f6e7a")
+        this.cameras.main.setBackgroundColor("0x5f6e7a");
     }
-
+ 
     preload() {
         console.log('Game Scene')
 
-        // images
+
+        //image
         this.load.image('starBackground', 'assets/starBackground.png')
         this.load.image('ship', 'assets/spaceShip.png')
         this.load.image('missile', 'assets/missile.png')
         this.load.image('alien', 'assets/alien.png')
-
-        // sounds
+        // sound
         this.load.audio('laser', 'assets/laser1.wav')
         this.load.audio('explosion', 'assets/barrelExploding.wav')
         this.load.audio('bomb', 'assets/bomb.wav')
     }
-
+ 
     create(data) {
         this.isGameOver = false
         this.fireMissile = false
         this.score = 0
 
+
         this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
         this.background.setOrigin(0, 0)
 
+
         this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
+
 
         this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
 
-        this.missileGroup = this.physics.add.group()
-        this.alienGroup = this.physics.add.group() 
 
+        // create a group for the missiles
+        this.missileGroup = this.physics.add.group()
+
+
+        // create a group for the alines
+        this.alienGroup = this.add.group()
         this.createAlien()
 
-        // Missiles hit aliens
-        this.physics.add.overlap(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
-            missileCollide.destroy()
-            alienCollide.health -= this.missileDamage
 
-            if (alienCollide.health <= 0) {
-                alienCollide.destroy()
-                this.sound.play('explosion')
-                this.score += 1
-                this.scoreText.setText('Score: ' + this.score.toString())
-                this.createAlien()
-                this.createAlien()
-            }
+        // Collisions between missiles and alines
+        this.physics.add.overlap(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+            alienCollide.destroy()
+            missileCollide.destroy()
+            this.sound.play('explosion')
+            this.score = this.score + 1
+            this.scoreText.setText('Score: ' + this.score.toString())
+            this.createAlien()
+            this.createAlien()
         }.bind(this))
 
-        // Aliens hit ship
+
+        // Collisions between ship and aliens
         this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
             this.sound.play('bomb')
             this.physics.pause()
@@ -102,36 +107,57 @@ class GameScene extends Phaser.Scene {
             this.gameOverText.on('pointerdown', () => this.scene.restart())
         }.bind(this))
     }
-
+ 
     update(time, delta) {
+       
         const keyLeftObj = this.input.keyboard.addKey('LEFT')
         const keyRightObj = this.input.keyboard.addKey('RIGHT')
         const keyUpObj = this.input.keyboard.addKey('UP')
         const keyDownObj = this.input.keyboard.addKey('DOWN')
         const keySpaceObj = this.input.keyboard.addKey('SPACE')
 
-        if (keyUpObj.isDown) {
+
+
+
+        if (keyUpObj.isDown === true) {
             this.ship.y -= 15
-            if (this.ship.y < 0) this.ship.y = 1080
+            if (this.ship.y < 0) {
+                this.ship.y = 1080
+            }
         }
 
-        if (keyDownObj.isDown) {
+
+
+
+
+
+        if (keyDownObj.isDown === true) {
             this.ship.y += 15
-            if (this.ship.y > 1080) this.ship.y = 0
+            if (this.ship.y > 1080) {
+                this.ship.y = 0
+            }
         }
 
-        if (keyLeftObj.isDown) {
+
+
+
+        if (keyLeftObj.isDown === true) {
             this.ship.x -= 15
-            if (this.ship.x < 0) this.ship.x = 1920
+            if (this.ship.x < 0) {
+                this.ship.x = 1920
+            }
         }
-
-        if (keyRightObj.isDown) {
+       
+        if (keyRightObj.isDown === true) {
             this.ship.x += 15
-            if (this.ship.x > 1920) this.ship.x = 0
+            if (this.ship.x > 1920) {
+                this.ship.x = 0
+            }
         }
 
-        if (keySpaceObj.isDown) {
-            if (!this.fireMissile && !this.isGameOver) {
+
+        if (keySpaceObj.isDown === true) {
+            if (this.fireMissile === false && !this.isGameOver) {
                 this.fireMissile = true
                 const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
                 this.missileGroup.add(aNewMissile)
@@ -139,15 +165,18 @@ class GameScene extends Phaser.Scene {
             }
         }
 
-        if (keySpaceObj.isUp) {
+
+        if (keySpaceObj.isUp === true) {
             this.fireMissile = false
         }
 
+
         this.missileGroup.children.each(function (item) {
-            item.y -= 15
-            if (item.y < 0) item.destroy()
+            item.y = item.y - 15
+            if (item.y < 0) {
+                item.destroy()
+            }
         })
     }
-}
-
-export default GameScene
+}    
+    export default GameScene
